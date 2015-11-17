0f491bbc06d818d9360e59f6e7d6939898f23797
Bastiaan Steinmeier <bastiaan.steinmeier@ultimo.net>
2015-11-17 16:38:31 +0100

    Initial commit

---
+++ SimpleSTACK/Form1.Designer.cs
@@ -0,0 +1,175 @@
+﻿namespace SimpleSTACK
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.password = new System.Windows.Forms.TextBox();
+            this.userName = new System.Windows.Forms.TextBox();
+            this.btnConnect = new System.Windows.Forms.Button();
+            this.filesTree = new System.Windows.Forms.TreeView();
+            this.textFreeSpace = new System.Windows.Forms.TextBox();
+            this.uploadProgress = new System.Windows.Forms.ProgressBar();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textUploadSpeed = new System.Windows.Forms.TextBox();
+            this.stackUrl = new System.Windows.Forms.TextBox();
+            this.toolTip1 = new System.Windows.Forms.ToolTip(this.components);
+            this.SuspendLayout();
+            // 
+            // password
+            // 
+            this.password.Location = new System.Drawing.Point(199, 36);
+            this.password.Name = "password";
+            this.password.PasswordChar = '*';
+            this.password.Size = new System.Drawing.Size(189, 20);
+            this.password.TabIndex = 2;
+            // 
+            // userName
+            // 
+            this.userName.Location = new System.Drawing.Point(12, 35);
+            this.userName.Name = "userName";
+            this.userName.Size = new System.Drawing.Size(181, 20);
+            this.userName.TabIndex = 1;
+            // 
+            // btnConnect
+            // 
+            this.btnConnect.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnConnect.Location = new System.Drawing.Point(394, 33);
+            this.btnConnect.Name = "btnConnect";
+            this.btnConnect.Size = new System.Drawing.Size(114, 23);
+            this.btnConnect.TabIndex = 3;
+            this.btnConnect.Text = "Connect";
+            this.btnConnect.UseVisualStyleBackColor = true;
+            this.btnConnect.Click += new System.EventHandler(this.btnConnect_Click);
+            // 
+            // filesTree
+            // 
+            this.filesTree.AllowDrop = true;
+            this.filesTree.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.filesTree.Location = new System.Drawing.Point(12, 61);
+            this.filesTree.Name = "filesTree";
+            this.filesTree.Size = new System.Drawing.Size(496, 303);
+            this.filesTree.TabIndex = 4;
+            // 
+            // textFreeSpace
+            // 
+            this.textFreeSpace.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textFreeSpace.Location = new System.Drawing.Point(420, 377);
+            this.textFreeSpace.Name = "textFreeSpace";
+            this.textFreeSpace.ReadOnly = true;
+            this.textFreeSpace.Size = new System.Drawing.Size(52, 22);
+            this.textFreeSpace.TabIndex = 6;
+            this.textFreeSpace.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // uploadProgress
+            // 
+            this.uploadProgress.Location = new System.Drawing.Point(12, 376);
+            this.uploadProgress.Name = "uploadProgress";
+            this.uploadProgress.Size = new System.Drawing.Size(269, 23);
+            this.uploadProgress.Step = 1;
+            this.uploadProgress.TabIndex = 7;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(376, 380);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(44, 16);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "Free:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(478, 380);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(29, 16);
+            this.label2.TabIndex = 9;
+            this.label2.Text = "GB";
+            // 
+            // textUploadSpeed
+            // 
+            this.textUploadSpeed.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textUploadSpeed.Location = new System.Drawing.Point(287, 377);
+            this.textUploadSpeed.Name = "textUploadSpeed";
+            this.textUploadSpeed.ReadOnly = true;
+            this.textUploadSpeed.Size = new System.Drawing.Size(83, 22);
+            this.textUploadSpeed.TabIndex = 10;
+            this.textUploadSpeed.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // stackUrl
+            // 
+            this.stackUrl.Location = new System.Drawing.Point(12, 9);
+            this.stackUrl.Name = "stackUrl";
+            this.stackUrl.Size = new System.Drawing.Size(376, 20);
+            this.stackUrl.TabIndex = 11;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(520, 401);
+            this.Controls.Add(this.stackUrl);
+            this.Controls.Add(this.textUploadSpeed);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.uploadProgress);
+            this.Controls.Add(this.textFreeSpace);
+            this.Controls.Add(this.filesTree);
+            this.Controls.Add(this.btnConnect);
+            this.Controls.Add(this.userName);
+            this.Controls.Add(this.password);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "SimpleSTACK";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.TextBox password;
+        private System.Windows.Forms.TextBox userName;
+        private System.Windows.Forms.Button btnConnect;
+        private System.Windows.Forms.TreeView filesTree;
+        private System.Windows.Forms.TextBox textFreeSpace;
+        private System.Windows.Forms.ProgressBar uploadProgress;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textUploadSpeed;
+        private System.Windows.Forms.TextBox stackUrl;
+        private System.Windows.Forms.ToolTip toolTip1;
+    }
+}
+
---
+++ SimpleSTACK/Form1.cs
@@ -0,0 +1,220 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml;
+using System.Security.Cryptography;
+
+namespace SimpleSTACK
+{
+    public partial class Form1 : Form
+    {
+        string userAgent = "SimpleSTACK v1.0 - info@nednu.nl";
+        static byte[] s_aditionalEntropy = { 9, 8, 7, 6, 5 };
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            filesTree.AllowDrop = true;
+            filesTree.DragEnter += filesTree_DragEnter;
+            filesTree.DragDrop += filesTree_DragDrop;
+            filesTree.MouseEnter += new System.EventHandler(filesTree_MouseEnter);
+        }
+
+
+        private void filesTree_DragEnter(object sender, DragEventArgs e)
+        {
+            e.Effect = DragDropEffects.Move;
+        }
+
+        private async void filesTree_DragDrop(object sender, DragEventArgs e)
+        {
+            string[] files = (string[])e.Data.GetData(DataFormats.FileDrop, false);
+            foreach (String file in files)
+            {
+                //Upload dropped file
+                await UploadFileAsync(file);
+            }
+        }
+
+        private void filesTree_MouseEnter(object sender, System.EventArgs e)
+        {
+                toolTip1.SetToolTip(this.filesTree, "Drop file(s) here to uploads");
+        }
+
+        private void ManipulateUI(Action action)
+        {
+            this.Invoke(action);
+        }
+
+
+        //Encrypt password in Config-file
+        private string Crypt(string text)
+        {
+            return Convert.ToBase64String(
+                ProtectedData.Protect(
+                    Encoding.Unicode.GetBytes(text), s_aditionalEntropy, DataProtectionScope.CurrentUser));
+        }
+        //Decrypt password from Config-file
+        private string Decrypt(string text)
+        {
+            return Encoding.Unicode.GetString(
+                ProtectedData.Unprotect(
+                     Convert.FromBase64String(text), s_aditionalEntropy, DataProtectionScope.CurrentUser));
+        }
+
+        public async Task<HttpResponseMessage> UploadFileAsync(string absoluteFilePath)
+        {
+            var length = new FileInfo(absoluteFilePath).Length;
+            var fileName = new FileInfo(absoluteFilePath).Name;
+
+            string url = stackUrl.Text + fileName;
+            string credentials = userName.Text + ":" + password.Text;
+
+            try
+            {
+                WebRequest uploadRequest = WebRequest.Create(url);
+                uploadRequest.Headers["UserAgent"] = userAgent;
+                uploadRequest.Method = "PUT";
+                uploadRequest.Headers["Authorization"] = "Basic " + Convert.ToBase64String(Encoding.ASCII.GetBytes(credentials));
+                uploadRequest.ContentLength = length;
+                const int chunkSize = 2048;
+                var buffer = new byte[chunkSize];
+                TimeSpan elapsedTime = TimeSpan.Zero;
+
+                using (var req = await uploadRequest.GetRequestStreamAsync())
+                using (var readStream = File.OpenRead(absoluteFilePath))
+                {
+                    ManipulateUI(() => this.uploadProgress.Value = 0);
+                    int read = 0;
+                    DateTime started = DateTime.Now;
+                    for (int i = 0; i < length; i += read)
+                    {
+                        read = await readStream.ReadAsync(buffer, 0, chunkSize);
+                        await req.WriteAsync(buffer, 0, read);
+                        await req.FlushAsync(); 
+                        ManipulateUI(() => this.uploadProgress.Value = (int)(100.0 * i / length));
+                    }
+                    elapsedTime = DateTime.Now - started;
+                    ManipulateUI(() => this.uploadProgress.Value = 0);
+                    ManipulateUI(() => this.textUploadSpeed.Text = Convert.ToString(Math.Round((((length / 1024) / elapsedTime.TotalSeconds) / 1024),2)+" MB/s"));
+                }
+
+                var response = (HttpWebResponse)await uploadRequest.GetResponseAsync();
+                var result = new HttpResponseMessage(response.StatusCode);
+                result.Content = new StreamContent(response.GetResponseStream());
+                getFiles();
+
+                return result;
+
+            }
+            catch (WebException wEx)
+            {
+                MessageBox.Show(Convert.ToString(wEx.Status));
+                return null;
+            }catch (Exception Ex)
+            {
+                MessageBox.Show(Convert.ToString(Ex.Message));
+                return null;
+            }
+        }
+
+        private void getFiles()
+        {
+            string url = stackUrl.Text;
+            string credentials = userName.Text + ":" + password.Text;
+            toolTip1.Active = false;
+
+            //Connect and get files
+            try
+            {
+                WebRequest filesRequest = WebRequest.Create(url);
+                filesRequest.Headers["UserAgent"] = userAgent;
+                filesRequest.Method = "PROPFIND";
+                filesRequest.Headers["Authorization"] = "Basic " + Convert.ToBase64String(Encoding.ASCII.GetBytes(credentials));
+                filesRequest.Timeout = 10000; //10 seconds
+
+           
+                WebResponse wr = filesRequest.GetResponse();
+                Stream receiveStream = wr.GetResponseStream();
+                StreamReader reader = new StreamReader(receiveStream, Encoding.UTF8);
+                string content = reader.ReadToEnd();
+
+                string filesPat = "(?<=<d:href>)(.*?)(?=</d:href>)";
+                string availableBytesPat = "(?<=<d:quota-available-bytes>)(.*?)(?=</d:quota-available-bytes>)";
+                MatchCollection matchFiles = Regex.Matches(content, filesPat);
+                MatchCollection matchAvailableBytes = Regex.Matches(content, availableBytesPat);
+
+                textFreeSpace.Text = Convert.ToString(Math.Round(((Convert.ToDouble(matchAvailableBytes[1].Value) / 1024) / 1024) / 1024));
+
+                filesTree.Nodes.Clear();
+                foreach (Match match in matchFiles)
+                {
+                    if (match.Value != "/remote.php/webdav/")
+                    {
+                        filesTree.BeginUpdate();
+                        string fileParentNode;
+                        fileParentNode = match.Value.Replace("/remote.php/webdav/", "").Trim();
+                        filesTree.Nodes.Add(fileParentNode);
+                        filesTree.EndUpdate();
+                    }
+                }
+
+            }catch(WebException wEx)
+            {
+                MessageBox.Show(Convert.ToString(wEx.Status));
+            }catch(Exception Ex)
+            {
+                MessageBox.Show(Convert.ToString(Ex.Message));
+            }
+}
+
+
+        private void btnConnect_Click(object sender, EventArgs e)
+        {
+            //Save settings in Config-file
+            XmlDocument xmlDoc = new XmlDocument();
+            xmlDoc.Load("Config.xml");
+
+            XmlElement urlElement = (XmlElement)xmlDoc.SelectSingleNode("/config/url");
+            XmlElement usernameElement = (XmlElement)xmlDoc.SelectSingleNode("/config/username");
+            XmlElement passwordElement = (XmlElement)xmlDoc.SelectSingleNode("/config/password");
+
+            if (urlElement != null && usernameElement != null && passwordElement != null)
+            {
+                usernameElement.SetAttribute("value", userName.Text); 
+                passwordElement.SetAttribute("value", Crypt(password.Text));
+                urlElement.SetAttribute("value", stackUrl.Text);
+            }
+            xmlDoc.Save("Config.xml");
+
+            getFiles();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //Load settings from Config-file
+            XmlDocument xmlDoc = new XmlDocument();
+            xmlDoc.Load("Config.xml");
+
+            XmlElement urlElement = (XmlElement)xmlDoc.SelectSingleNode("/config/url");
+            XmlElement usernameElement = (XmlElement)xmlDoc.SelectSingleNode("/config/username");
+            XmlElement passwordElement = (XmlElement)xmlDoc.SelectSingleNode("/config/password");
+
+            if (urlElement != null && usernameElement != null && passwordElement != null)
+            {
+                stackUrl.Text = urlElement.GetAttribute("value");
+                userName.Text = usernameElement.GetAttribute("value");
+                password.Text = Decrypt(passwordElement.GetAttribute("value"));
+                
+            }
+
+        }
+
+    }
+}
